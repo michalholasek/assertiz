2f868b7c35e8b897dba52054e495f6429ee7f3bf
Michal Holasek <isindzr@gmail.com>
2014-08-12 21:14:14 +0200

    Modular system is now more 'node friendly'

--- src/assert.js
+++ src/assert.js
@@ -1,47 +1,49 @@
 (function () {
   'use strict';
 
-  module('assert', function () {
-    var assert = {};
-    var fail;
+  var assert = {};
+  var fail;
 
-    assert.clear = function () {
-      if (assert.error) {
-        delete assert.error;
+  assert.clear = function () {
+    if (assert.error) {
+      delete assert.error;
+    }
+  };
+
+  assert.equal = function (actual, expected) {
+    if (actual !== expected) {
+      fail(actual + ' is not equal to ' + expected);
+    }
+  };
+
+  assert.throws = function (fn, comparer) {
+    try {
+      if (!isFunction(fn)) {
+        fail('fn is not a function');
+      } else {
+        fn();
+        fail('function did not throw an error');
       }
-    };
-
-    assert.equal = function (actual, expected) {
-      if (actual !== expected) {
-        fail(actual + ' is not equal to ' + expected);
+    } catch (err) {
+      if (isFunction(comparer) && !comparer(err)) {
+        fail('comparer function did not return true');
       }
-    };
+    }
+  };
 
-    assert.throws = function (fn, comparer) {
-      try {
-        if (!isFunction(fn)) {
-          fail('fn is not a function');
-        } else {
-          fn();
-          fail('function did not throw an error');
-        }
-      } catch (err) {
-        if (isFunction(comparer) && !comparer(err)) {
-          fail('comparer function did not return true');
-        }
-      }
-    };
+  assert.true = function (value) {
+    if (typeof value !== 'boolean' || !value) {
+      fail('value is not true');
+    }
+  };
 
-    assert.true = function (value) {
-      if (typeof value !== 'boolean' || !value) {
-        fail('value is not true');
-      }
-    };
-
-    fail = function (message) {
-      assert.error = new Error(message);
-    };
-    
-    module.exports = assert;
-  });
+  fail = function (message) {
+    assert.error = new Error(message);
+  };
+  
+  if (!isUndefined(window)) {
+    module.register('assert', assert);
+  } else {
+    module.exports = assert;  
+  }
 }());
--- src/assertiz.js
+++ src/assertiz.js
@@ -1,202 +1,204 @@
 (function () {
   'use strict';
 
-  module('assertiz', function () {
-    var assert = require('assert');
-    var listenerId = -1;
-    var listeners = {};
-    var paused = false;
-    var onFinished;
-    var suite = '';
-    var queue = [];
-    var assertiz;
-    var runAsync;
-    var addTest;
-    var onError;
-    var runSync;
-    var runTest;
-    var emit;
-    var next;
-    var Test;
+  var assert = require('assert');
+  var listenerId = -1;
+  var listeners = {};
+  var paused = false;
+  var onFinished;
+  var suite = '';
+  var queue = [];
+  var assertiz;
+  var runAsync;
+  var addTest;
+  var onError;
+  var runSync;
+  var runTest;
+  var emit;
+  var next;
+  var Test;
 
-    //----------------------------------------------------------------------
-    // Runner
-    //----------------------------------------------------------------------
+  //----------------------------------------------------------------------
+  // Runner
+  //----------------------------------------------------------------------
 
-    addTest = function (test) {
-      queue.push(test);
-    };
+  addTest = function (test) {
+    queue.push(test);
+  };
 
-    emit = function (event, test) {
-      if (!listeners[event]) return;
+  emit = function (event, test) {
+    if (!listeners[event]) return;
 
-      for (var i = 0; i < listeners[event].length; i++) {
-        listeners[event][i].fn(test);
-      }
-    };
+    for (var i = 0; i < listeners[event].length; i++) {
+      listeners[event][i].fn(test);
+    }
+  };
 
-    onError = function (test) {
-      emit('test-error', test);
-      next();
-    };
+  onError = function (test) {
+    emit('test-error', test);
+    next();
+  };
 
-    onFinished = function (test) {
-      emit('test-finished', test);
-      next();
-    };
+  onFinished = function (test) {
+    emit('test-finished', test);
+    next();
+  };
 
-    next = function () {
-      paused = false;
-      setTimeout(assertiz.run, 1);
-    };
+  next = function () {
+    paused = false;
+    setTimeout(assertiz.run, 1);
+  };
 
-    runTest = function () {
-      var test;
+  runTest = function () {
+    var test;
 
-      if (!paused && queue.length) {
-        test = queue.shift();
-        test.run(onFinished, onError);
-      }
-    };
+    if (!paused && queue.length) {
+      test = queue.shift();
+      test.run(onFinished, onError);
+    }
+  };
 
-    //----------------------------------------------------------------------
-    // Test
-    //----------------------------------------------------------------------
+  //----------------------------------------------------------------------
+  // Test
+  //----------------------------------------------------------------------
 
-    Test = function (suite, name, fn, async) {
-      this.async = async || false;
-      this.suite = suite;
-      this.name = name;
-      this.fn = fn;
-    };
+  Test = function (suite, name, fn, async) {
+    this.async = async || false;
+    this.suite = suite;
+    this.name = name;
+    this.fn = fn;
+  };
 
-    Test.prototype.run = function (onFinished, onError) {
-      if (this.async) {
-        runAsync(this, onFinished, onError);
-      } else {
-        runSync(this, onFinished, onError);
-      }
-    };
+  Test.prototype.run = function (onFinished, onError) {
+    if (this.async) {
+      runAsync(this, onFinished, onError);
+    } else {
+      runSync(this, onFinished, onError);
+    }
+  };
 
-    runAsync = function (test, onFinished, onError) {
-      var start = Date.now();
-      var timeout = 3000;
-      var timer;
+  runAsync = function (test, onFinished, onError) {
+    var start = Date.now();
+    var timeout = 3000;
+    var timer;
 
-      timer = setTimeout(function canceled() {
-          test.duration = Date.now() - start;
-          test.error = new Error('test timed out');
-          test.canceled = true;
-          test.passed = false;
-          
-          onError(test);
-        }, timeout);
-
-      test.fn(function done() {
-        if (test.canceled) { return; }
-
-        clearTimeout(timer);
-
+    timer = setTimeout(function canceled() {
         test.duration = Date.now() - start;
+        test.error = new Error('test timed out');
+        test.canceled = true;
+        test.passed = false;
+        
+        onError(test);
+      }, timeout);
 
-        if (!assert.error) {
-          test.passed = true;
+    test.fn(function done() {
+      if (test.canceled) { return; }
 
-          onFinished(test);
+      clearTimeout(timer);
 
-        } else {
-          test.passed = false;
-          test.error = assert.error;
-
-          assert.clear();
-
-          onError(test);
-        }
-      });
-    };
-
-    runSync = function (test, onFinished, onError) {
-      var start = Date.now();
-
-      test.fn();
       test.duration = Date.now() - start;
 
       if (!assert.error) {
         test.passed = true;
+
         onFinished(test);
+
       } else {
         test.passed = false;
         test.error = assert.error;
 
         assert.clear();
+
         onError(test);
       }
-    };
+    });
+  };
 
-    //----------------------------------------------------------------------
-    // Public Interface
-    //----------------------------------------------------------------------
+  runSync = function (test, onFinished, onError) {
+    var start = Date.now();
 
-    assertiz = {
+    test.fn();
+    test.duration = Date.now() - start;
 
-      off: function (token) {
-        if (!isString(token)) return;
+    if (!assert.error) {
+      test.passed = true;
+      onFinished(test);
+    } else {
+      test.passed = false;
+      test.error = assert.error;
 
-        for (var event in listeners) {
-          if (listeners[event]) {
-            for (var i = 0; i < listeners[event].length; i++) {
-              if (listeners[event][i].token === token) {
-                listeners[event].splice(i, 1);
-                listenerId--;
-                return token;
-              }
+      assert.clear();
+      onError(test);
+    }
+  };
+
+  //----------------------------------------------------------------------
+  // Public Interface
+  //----------------------------------------------------------------------
+
+  assertiz = {
+
+    off: function (token) {
+      if (!isString(token)) return;
+
+      for (var event in listeners) {
+        if (listeners[event]) {
+          for (var i = 0; i < listeners[event].length; i++) {
+            if (listeners[event][i].token === token) {
+              listeners[event].splice(i, 1);
+              listenerId--;
+              return token;
             }
           }
         }
-
-        return false;
-      },
-
-      on: function (event, fn) {
-        var token = '';
-
-        if (!isString(event) || !isFunction(fn)) return;
-
-        if (!listeners[event]) {
-          listeners[event] = [];
-        }
-
-        token = (++listenerId).toString();
-        listeners[event].push({
-          token: token,
-          fn: fn
-        });
-
-        return token;
-      },
-      
-      run: function () {
-        runTest();
-      },
-
-      suite: function (name, fn) {
-        suite = isString(name) ? name : '';
-        fn();
-        suite = '';
-      },
-
-      test: function (name, fn, async) {
-        // Push only valid test
-        if (!isString(name) || !isFunction(fn) ||
-           (async && typeof async !== 'boolean')) {
-          return;
-        }
-        
-        addTest(new Test(suite, name, fn, async));
       }
 
-    };
+      return false;
+    },
 
-    module.exports = assertiz;
-  });
+    on: function (event, fn) {
+      var token = '';
+
+      if (!isString(event) || !isFunction(fn)) return;
+
+      if (!listeners[event]) {
+        listeners[event] = [];
+      }
+
+      token = (++listenerId).toString();
+      listeners[event].push({
+        token: token,
+        fn: fn
+      });
+
+      return token;
+    },
+    
+    run: function () {
+      runTest();
+    },
+
+    suite: function (name, fn) {
+      suite = isString(name) ? name : '';
+      fn();
+      suite = '';
+    },
+
+    test: function (name, fn, async) {
+      // Push only valid test
+      if (!isString(name) || !isFunction(fn) ||
+         (async && typeof async !== 'boolean')) {
+        return;
+      }
+      
+      addTest(new Test(suite, name, fn, async));
+    }
+
+  };
+
+  if (!isUndefined(window)) {
+    module.register('assertiz', assertiz);
+  } else {
+    module.exports = assertiz;  
+  }
 }());
